01041b991c2801aa828ae9e721766435da8909b0
PUP-Loki <75944209+PUP-Loki@users.noreply.github.com>
2021-01-09 07:04:15 +0000

    Add files via upload
    setup script ubuntu

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get install build-essential rsync texinfo libncurses-dev whois unzip bc qt4-linguist-tools libssl-dev git bsdtar qt4-dev-tools libqt4-dev libqt4-core libqt4-gui python ca-certificates -y
+wget https://www.libarchive.org/downloads/libarchive-3.3.1.tar.gz
+tar xzf libarchive-3.3.1.tar.gz
+cd libarchive-3.3.1
+./configure
+make
+make install
+git clone https://github.com/PUP-Loki/pinn.git
+cd /root/pinn/
+./BUILDME.sh
